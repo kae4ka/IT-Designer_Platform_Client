46737f4ce5d16c61999fff4781639c9182ae853f
sletkov <70477360+sletkov@users.noreply.github.com>
2022-01-19 23:42:37 +0300

    Добавлен функционал детального просмотра проекта

--- src/pages/location/Location.js
+++ src/pages/location/Location.js
@@ -1,122 +1,58 @@
-import { Component, useState, useEffect } from "react";
-import { PROJECTS } from '../../shared/projects';
-import './Location.scss';
+import { useState, useEffect } from "react";
+import { PROJECTS } from "../../shared/projects";
+import "./Location.scss";
 
-
-const ShowProjectDetails = ({project, isPopupOpen}) => {
-    console.log(project);
-    if(project != undefined) {
-        return(
-            <div className="project-detail" visibility={isPopupOpen}>
-                <img src={project.image} alt={project.title} />
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
-            </div>
-        );
-    } else {
-        return(
-            <div>
-                Тут пусто
-            </div>
-        );
-    }
-}
+const ShowProjectDetails = ({ project }) => {
+    return project !== undefined ? (
+        <div className="project-detail">
+            <img src={project.image} alt={project.title} />
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+        </div>
+    ) : (
+        <div></div>
+    );
+};
 
 export default function Location() {
-    const [prjcts,setPrjcts] = useState([]);
-    const [isPopupOpen,setIsPopupOpen] = useState(false);
-
-    let clickedProjectId = null;
+    const [prjcts, setPrjcts] = useState([]);
+    const [clickedProjectId, setClickedProjectId] = useState();
 
     useEffect(() => {
         setPrjcts(PROJECTS);
-    });
+    }, []);
 
-    return(
-
+    return (
         <div className="location">
             <h2 className="location-heading">Грузия</h2>
-            {
-            (prjcts != null)? 
-            (
+            {prjcts != null ? (
                 <div className="projects-container">
-                    {
-                        prjcts.map((project)=> {
-                            return(
-                                <div key={project.id} className="project-card">
-                                    <img src={project.image} alt={project.title} className="project-image" onClick={() => {
-                                        clickedProjectId = project.id
-                                        setIsPopupOpen(true);
-                                    }
-                                        }/>
-                                </div>
-                            );
-                        })
-                    }
+                    {prjcts.map((project) => {
+                        return (
+                            <div key={project.id} className="project-card">
+                                <img
+                                    src={project.image}
+                                    alt={project.title}
+                                    className="project-image"
+                                    onClick={() => {
+                                        setClickedProjectId(project.id);
+                                    }}
+                                />
+                            </div>
+                        );
+                    })}
 
-                    <div className="project-details">
-                        <button onClick={() => console.log(clickedProjectId)}>Show current Id</button>
-                        <ShowProjectDetails project={prjcts[clickedProjectId]} isPopupOpen={isPopupOpen}/>
+                    <div className="project-details">   
+                        <ShowProjectDetails
+                            project={prjcts[clickedProjectId]}
+                        />
                     </div>
                 </div>
-            ):
-            (
+            ) : (
                 <div>
                     <p>Здесь пока нет проектов</p>
                 </div>
-            )
-        }
+            )}
         </div>
     );
 }
-
-
-// class Location extends Component {
-
-//     constructor(props) {
-//         super(props);
-
-//         this.state = {
-//             projects: PROJECTS,
-//         }
-//     }
-
-//     renderProjects({projects}) {
-//         console.log(projects);
-//         if(projects != null) {
-//             return(
-//                 <div>
-//                     {
-//                         projects.map((project)=> {
-//                             return(
-//                                 <div key={project.id}>
-//                                     <h3>{project.title}</h3>
-//                                     <img src={project.image} alt={project.title}/>
-//                                 </div>
-//                             );
-//                         })
-//                     }
-//                 </div>
-//             );
-
-//         } else {
-//             return(
-//                 <div>
-//                     <p>Здесь пока нет проектов</p>
-//                 </div>
-//             );
-//         }
-//     }
-
-//     render() {
-//         return(
-//             <div className="Location">
-//                 <h2>Грузия</h2>
-//                 {this.renderProjects(this.state.projects)}
-//             </div>
-//         );
-//     }
-// }
-
-
-// export default Location;
